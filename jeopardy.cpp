4deff4cbaad5626888edfffc58a23c722fc22b81
thillux <theil.markus@gmail.com>
2016-12-11 13:05:45 +0100

    also use numbers as player keys

--- jeopardy.cpp
+++ jeopardy.cpp
@@ -210,7 +210,8 @@ bool Jeopardy::initPlayers()
 
     colorList << "red" << "green" << "yellow" << "blue" << "gray" << "magenta" << "darkRed" << "cyan" << "white" << "darkMagenta";
     keyList << "A" << "B" << "C" << "D" << "E" << "F" << "G" << "H" << "I" << "J" << "K" << "L" << "M"
-            << "N" << "O" << "P" << "Q" << "R" << "S" << "T" << "U" << "V" << "W" << "X" << "Y" << "Z";
+            << "N" << "O" << "P" << "Q" << "R" << "S" << "T" << "U" << "V" << "W" << "X" << "Y" << "Z"
+            << "0" << "1" << "2" << "3" << "4" << "5" << "6" << "7" << "8" << "9";
 
     for(this->playerNr = 0; this->playerNr < NUMBER_MAX_PLAYERS; this->playerNr++)
     {
